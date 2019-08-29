914fbd3970bc592ced5652acc7d7b73712b9b798
Nate Tan <nktan93@gmail.com>
2019-08-29 21:16:29 +0000

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function justInvoke(fn) {
 function setThisWithCall(fn, thisValue, arg) {
   return fn.call(thisValue, arg)
 }
+
+function setThisWithApply(fn, thisValue, args) {
+  return fn.apply(thisValue, args)
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  return functionToBeCopied.bind(thisValue)
+}
