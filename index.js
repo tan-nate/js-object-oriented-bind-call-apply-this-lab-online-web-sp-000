288c5541ff5bfb7b669cf2507e2f2e64ccf5c877
Nate Tan <nktan93@gmail.com>
2019-08-29 21:07:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function setThisWithCall(fn, thisValue, arg) {
 function setThisWithApply(fn, thisValue, args) {
   return fn.apply(thisValue, args)
 }
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  return fn.bind(thisValue)
+}
